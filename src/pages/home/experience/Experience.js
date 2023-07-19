4217617f9d9ca3848a9b25dfeaa5078ec7dcb364
pimenta <joao@excelsea.de>
2023-07-19 14:35:53 +0100

    Set experience content and accordion button

--- src/pages/home/experience/Experience.js
+++ src/pages/home/experience/Experience.js
@@ -1,18 +1,38 @@
 import React from 'react';
-import jobs from './jobs.json';
+import cv from './cv.json';
 
 // Components
 import ExperiencePanel from './experience-panel/ExperiencePanel';
+import WorkIcon from '../../../components/icons/WorkIcon';
+import EducationIcon from '../../../components/icons/EducationIcon';
+import VolunteerIcon from '../../../components/icons/VolunteerIcon';
 
 import './Experience.css';
 
 const Experience = () => {
-  console.log(jobs);
   return (
     <div className="experience" id="experience">
-      <ExperiencePanel title="Job Title" role="Role" />
-      <ExperiencePanel />
-      <ExperiencePanel />
+      <div className="experience-section">
+        <h2>WORK EXPERIENCE</h2>
+        <WorkIcon />
+      </div>
+      {cv.workExperience.map((item) => (
+        <ExperiencePanel key={item.subTitle} data={item} />
+      ))}
+      <div className="experience-section">
+        <h2>EDUCATION</h2>
+        <EducationIcon />
+      </div>
+      {cv.education.map((item) => (
+        <ExperiencePanel key={item.subTitle} data={item} />
+      ))}
+      <div className="experience-section">
+        <h2>VOLUNTEERING</h2>
+        <VolunteerIcon />
+      </div>
+      {cv.volunteering.map((item) => (
+        <ExperiencePanel key={item.subTitle} data={item} />
+      ))}
     </div>
   );
 };
--- src/pages/home/experience/experience-panel/ExperiencePanel.js
+++ src/pages/home/experience/experience-panel/ExperiencePanel.js
@@ -1,31 +1,53 @@
 import React, { useState } from 'react';
 
+import ChevronDownIcon from '../../../../components/icons/ChevronDownIcon';
+import ChevronUpIcon from '../../../../components/icons/ChevronUpIcon';
+
 import './ExperiencePanel.css';
 
-const ExperiencePanel = ({ title, role, description, stack }) => {
+const ExperiencePanel = ({ data }) => {
   const [isOpen, setPanelOpen] = useState(false);
+  const { title, subTitle, duration, imgPath, description } = data;
 
   const handleClick = () => {
     setPanelOpen(!isOpen);
   };
 
   return (
-    <div className="panel" onClick={handleClick}>
-      <h2>{title}</h2>
-      <h3>{role}</h3>
-      <div className={`accordion-content ${isOpen ? 'show-more' : ''}`}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed
-        imperdiet arcu. Vestibulum vel elit eget arcu feugiat ornare. Curabitur
-        quis blandit orci, sed laoreet sem. Pellentesque sit amet nibh turpis.
-        Mauris nec felis nibh. Vivamus mauris arcu, blandit eget tempor
-        ultricies, ullamcorper a orci. Etiam turpis nisi, fermentum vitae orci
-        non, hendrerit blandit massa. Sed porta urna eu mattis malesuada. Etiam
-        feugiat iaculis blandit. Proin varius sit amet ante ut luctus.
-        Suspendisse hendrerit luctus vehicula. Nunc rutrum augue eget pretium
-        ultrices. Etiam vitae odio tellus. Fusce facilisis metus ut velit
-        posuere, ac consequat justo condimentum. Praesent elementum posuere nunc
-        quis auctor. Sed dapibus elit a mi faucibus vulputate.
+    <div className="panel">
+      <div className="experience-info">
+        <div className="experience-img">
+          <img alt={subTitle} src={imgPath} />
+        </div>
+        <div className="experience-header">
+          <h3>{title}</h3>
+          <h4>{subTitle}</h4>
+          <h4>{duration}</h4>
+        </div>
+        {description && (
+          <div className="accordion-button" onClick={handleClick}>
+            {isOpen ? <ChevronUpIcon /> : <ChevronDownIcon />}
+          </div>
+        )}
       </div>
+      {description && (
+        <div className={`accordion-content ${isOpen ? 'show-more' : ''}`}>
+          <div className="experience-description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+            sed imperdiet arcu. Vestibulum vel elit eget arcu feugiat ornare.
+            Curabitur quis blandit orci, sed laoreet sem. Pellentesque sit amet
+            nibh turpis. Mauris nec felis nibh. Vivamus mauris arcu, blandit
+            eget tempor ultricies, ullamcorper a orci. Etiam turpis nisi,
+            fermentum vitae orci non, hendrerit blandit massa. Sed porta urna eu
+            mattis malesuada. Etiam feugiat iaculis blandit. Proin varius sit
+            amet ante ut luctus. Suspendisse hendrerit luctus vehicula. Nunc
+            rutrum augue eget pretium ultrices. Etiam vitae odio tellus. Fusce
+            facilisis metus ut velit posuere, ac consequat justo condimentum.
+            Praesent elementum posuere nunc quis auctor. Sed dapibus elit a mi
+            faucibus vulputate.
+          </div>
+        </div>
+      )}
     </div>
   );
 };
