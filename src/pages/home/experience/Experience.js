ede099c6acc6893096015bee7cfbb17d516537e7
pimenta <joao@excelsea.de>
2023-07-21 12:27:56 +0100

    Change theme styling

--- src/pages/home/experience/Experience.js
+++ src/pages/home/experience/Experience.js
@@ -13,21 +13,18 @@ const Experience = () => {
   return (
     <section className="section-experience" id="experience">
       <div className="experience-group">
-        <h2>WORK EXPERIENCE</h2>
         <WorkIcon />
       </div>
       {cv.workExperience.map((item) => (
         <ExperienceAccordion key={item.subTitle} data={item} />
       ))}
       <div className="experience-group">
-        <h2>EDUCATION</h2>
         <EducationIcon />
       </div>
       {cv.education.map((item) => (
         <ExperienceAccordion key={item.subTitle} data={item} />
       ))}
       <div className="experience-group">
-        <h2>VOLUNTEERING</h2>
         <VolunteerIcon />
       </div>
       {cv.volunteering.map((item) => (
